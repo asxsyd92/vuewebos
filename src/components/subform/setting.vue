b125c12965d88dc0ec5a60fa91da46f31f96b981
良富 陈 <asxsyd92@foxmail.com>
2021-08-18 22:35:47 +0800

    表单设计优化

--- src/components/subform/setting.vue
+++ src/components/subform/setting.vue
@@ -1,31 +1,31 @@
 <template>
-<div>
-<setinput v-if="data.type=='input'" :data="data"></setinput>
-<settextarea v-if="data.type=='textarea'" :data="data"></settextarea>
-</div>
+  <div>
+    <setinput v-if="data.type=='input'" :data="data"></setinput>
+    <settextarea v-if="data.type=='textarea'" :data="data"></settextarea>
+  </div>
 </template>
 <script>
-import webset from '@/components/subform/webset.js';
-export default {
-    components:{...webset},
- //   inject: ['settinglist'],  
-       props: {
-    data: {
-      type: Object,
-      required: true
-    },
-    model: Object
-  },    watch: {
-    'data': {
-      handler(v) {
-        console.log(v);
-        this.$emit('change', v);
+  import webset from '@/components/subform/webset.js';
+  export default {
+    components: { ...webset },
+    props: {
+      data: {
+        type: Object,
+        required: true
       },
-      deep: true
-    }},
-    mounted(){
+      model: Object
+    }, watch: {
+      'data': {
+        handler(v) {
+          console.log(v);
+          this.$emit('change', v);
+        },
+        deep: true
+      }
+    },
+    mounted() {
       debugger;
       console.log(this.data);
     }
-}
+  }
 </script>
