0e46b48495f1fc1430f507d8587262cfb69c21d5
陈良富 <chenlf@wscstudio.net>
2022-07-27 22:15:35 +0800

    优化radio

--- src/components/formitem/item/itemradio.vue
+++ src/components/formitem/item/itemradio.vue
@@ -4,8 +4,8 @@
 
 <div  :class="data.data.col"  :style="'display:'+data.data.display">
 
-    <lay-form-item :placeholder="data.data.placeholder" class="layui-form-item" :label="data.data.label" :prop="data.data.name">
-    <lay-radio v-for=" (li,index) in radio" :key="index" v-model="value[data.data.name]"   :label="li.value" >{{li.title}} </lay-radio>
+    <lay-form-item  class="layui-form-item" :label="data.data.label" :prop="data.data.name">
+    <lay-radio v-for=" (li,index) in radio" :key="index" v-model="value[data.data.name]" :name="data.data.name"  :value="li.value" >{{li.title}} </lay-radio>
     </lay-form-item>
 
 </div>
@@ -20,7 +20,7 @@ export default {
 </script>
 <script lang="ts" setup>
     import http from "../../../utils/http";
-    import { ref,reactive ,onMounted  } from "vue";
+    import { ref,reactive  } from "vue";
 interface ItemradioProps {
   data: any;
   value:any;
@@ -33,9 +33,9 @@ const data = ref(props.data);
 const value = ref(props.value);
 
 const radio=ref([]) as any;
-      onMounted(() => {
-        render();
-      });
+   
+     
+   
     const render = function () {
 
         //本地数据
@@ -91,6 +91,8 @@ const radio=ref([]) as any;
 
     }
     }
+
+       render();
 // import { ref,reactive ,onMounted,toRefs  } from "vue";
 // interface Data {
 //     [key:string]:null
