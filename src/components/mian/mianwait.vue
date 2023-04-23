18d00a3f95d96abc1f4ba44dc90cbdb43dcd2681
陈良富 <chenlf@wscstudio.net>
2023-04-23 16:32:54 +0800

    wait

--- src/components/mian/mianwait.vue
+++ src/components/mian/mianwait.vue
@@ -75,42 +75,42 @@ const waitcolumns = ref([
 
 const mianwait = ref([])
 nextTick(() => {
-  var chartDom = document.getElementById('main');
-  // @ts-ignore
-  var myChart = echarts.init(chartDom);
-  var option;
+  // var chartDom = document.getElementById('main');
+  // // @ts-ignore
+  // var myChart = echarts.init(chartDom);
+  // var option;
 
-  option = {
-    legend: {
-      data: ['Allocated Budget', 'Actual Spending']
-    },
-    radar: {
-      indicator: [
-        { name: '进攻', max: 6500 },
-        { name: '技巧', max: 16000 },
-        { name: '力量', max: 30000 },
-        { name: '速度', max: 38000 },
-        { name: '体能', max: 52000 },
-        { name: '防守', max: 25000 }
-      ]
-    },
-    series: [
-      {
-        name: 'Budget vs spending',
-        type: 'radar',
-        data: [
-          {
-            value: [4200, 3000, 20000, 35000, 50000, 18000],
-          },
-          {
-            value: [5000, 14000, 28000, 26000, 42000, 21000],
-          }
-        ]
-      }
-    ]
-  };
+  // option = {
+  //   legend: {
+  //     data: ['Allocated Budget', 'Actual Spending']
+  //   },
+  //   radar: {
+  //     indicator: [
+  //       { name: '进攻', max: 6500 },
+  //       { name: '技巧', max: 16000 },
+  //       { name: '力量', max: 30000 },
+  //       { name: '速度', max: 38000 },
+  //       { name: '体能', max: 52000 },
+  //       { name: '防守', max: 25000 }
+  //     ]
+  //   },
+  //   series: [
+  //     {
+  //       name: 'Budget vs spending',
+  //       type: 'radar',
+  //       data: [
+  //         {
+  //           value: [4200, 3000, 20000, 35000, 50000, 18000],
+  //         },
+  //         {
+  //           value: [5000, 14000, 28000, 26000, 42000, 21000],
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // };
 
-  option && myChart.setOption(option);
+  // option && myChart.setOption(option);
 })
 const editRowEvent = (row: any) => {
 
